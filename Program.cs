a1111477e993c4a51322d9b91b8c778bfa104c02
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-02-14 10:21:05 -0300

    Saindo da aplicacao

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ static void Menu()
     Console.WriteLine("2 - Subtração");
     Console.WriteLine("3 - Divisão");
     Console.WriteLine("4 - Multiplicação");
+    Console.WriteLine("5 - Sair");
 
     Console.WriteLine("----------");
     Console.WriteLine("Selecione uma opção: ");
@@ -22,6 +23,7 @@ static void Menu()
         case 2: Subtracao(); break;
         case 3: Divisao(); break;
         case 4: Multiplicacao(); break;
+        case 5: System.Environment.Exit(0); break;
         default: Menu(); break;
     }
 }
