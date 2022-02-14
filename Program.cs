bef9305262895e65296b2cb7c0de55972e4cb10b
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-02-14 09:50:28 -0300

    Adicionando função

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿Console.Clear();
 
-Console.WriteLine("Primeiro valor:");
-float v1 = float.Parse(Console.ReadLine());
+Soma();
 
-Console.WriteLine("Segundo valor: ");
-float v2 = float.Parse(Console.ReadLine());
 
-Console.WriteLine("");
+static void Soma()
+{
+    Console.WriteLine("Primeiro valor:");
+    float v1 = float.Parse(Console.ReadLine());
 
-float resultado = v1 + v2;
-Console.WriteLine("O resultado da soma é: " + resultado);
+    Console.WriteLine("Segundo valor: ");
+    float v2 = float.Parse(Console.ReadLine());
 
+    Console.WriteLine("");
+
+    float resultado = v1 + v2;
+    Console.WriteLine($"O resultado da soma é: {resultado}");
+}
