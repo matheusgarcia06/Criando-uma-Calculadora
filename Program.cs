b86a31b86220444c838048821d11ea26660fd748
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-02-14 09:46:35 -0300

    Adicionando a Soma

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Primeiro valor:");
+float v1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Segundo valor: ");
+float v2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("");
+
+float resultado = v1 + v2;
+Console.WriteLine("O resultado da soma é: " + resultado);
+
